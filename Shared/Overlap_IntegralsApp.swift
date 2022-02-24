73c73eed0091c8a85963662baa7568a4522b4271
jterry94 <terryj@iit.edu>
2022-02-23 22:45:39 -0600

    Initial Commit

---
+++ Shared/Overlap_IntegralsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Overlap_IntegralsApp.swift
+//  Shared
+//
+//  Created by Jeff_Terry on 2/23/22.
+//
+
+import SwiftUI
+
+@main
+struct Overlap_IntegralsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
